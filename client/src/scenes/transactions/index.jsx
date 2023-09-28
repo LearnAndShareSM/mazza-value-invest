8bd950addb8b0209ff33c08634c9cea413d8ee3e
mazzasaverio <saverio3107@gmail.com>
2023-09-28 23:47:31 +0200

    UPDATE: datagrid

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -1,125 +1,7 @@
-// import React, { useState } from "react";
-// import { Box, useTheme } from "@mui/material";
-// import { DataGrid } from "@mui/x-data-grid";
-// import { useGetTransactionsQuery } from "state/api";
-// import Header from "components/Header";
-// import DataGridCustomToolbar from "components/DataGridCustomToolbar";
-
-// const Transactions = () => {
-//   const theme = useTheme();
-
-//   // values to be sent to the backend
-//   const [page, setPage] = useState(0);
-//   const [pageSize, setPageSize] = useState(20);
-//   const [sort, setSort] = useState({});
-//   const [search, setSearch] = useState("");
-
-//   const [searchInput, setSearchInput] = useState("");
-//   const { data, isLoading } = useGetTransactionsQuery({
-//     page,
-//     pageSize,
-//     sort: JSON.stringify(sort),
-//     search,
-//   });
-
-//   const columns = [
-//     {
-//       field: "_id",
-//       headerName: "ID",
-//       flex: 1,
-//     },
-//     {
-//       field: "userId",
-//       headerName: "User ID",
-//       flex: 1,
-//     },
-//     {
-//       field: "createdAt",
-//       headerName: "Created At",
-//       flex: 1,
-//     },
-//     {
-//       field: "products",
-//       headerName: "# of Products",
-//       flex: 0.5,
-//       sortable: false,
-//       renderCell: (params) => {
-//         return params.value.length;
-//       },
-//     },
-//     {
-//       field: "cost",
-//       headerName: "Cost",
-//       flex: 1,
-//       renderCell: (params) => { 
-//         return `$${Number(params.value).toFixed(2)}`;
-//       }
-      
-//     },
-
-//   ];
-
-//   return <Box m="1.5rem 2.5rem">
-// <Header title="Transactions" subtitle="List of Transactions" />
-// <Box
-//  height="80vh"
-//  sx={{
-//    "& .MuiDataGrid-root": {
-//      border: "none",
-//    },
-//    "& .MuiDataGrid-cell": {
-//      borderBottom: "none",
-//    },
-//    "& .MuiDataGrid-columnHeaders": {
-//      backgroundColor: theme.palette.primary.main,
-//      color: theme.palette.secondary[100],
-//      borderBottom: "none"
-//    },
-//    "& .MuiDataGrid-footerContainer": {
-//      backgroundColor: theme.palette.primary.light,
-//      color: theme.palette.secondary[100],
-//      borderTop: "none"
-//    },
-//    "& .MuiDataGrid-toolbarCointainer .MuiButton-text": {
-//      color: `{theme.palette.secondary[200]} !important`
-//    }
- 
- 
-//  }} 
-// >
-//   <DataGrid 
-//   loading={isLoading || !data}
-//   getRowId={(row) => row._id}
-//   rows={data && data.transactions || []}
-//   columns = {columns}
-//   rowCount={data && data.total || 0}
-//   rowsPerPageOptions={[20, 50, 100]}
-//   pagination
-//   page = {page}
-//   pageSize={pageSize}
-//   paginationMode="server"
-//   sortingMode="server"
-//   onPageChange={(newPage) => setPage(newPage)}
-//   onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-//   onSortModelChange= {(newSortModel) => setSort(...newSortModel)}
-//   slots={{
-//     toolbar: DataGridCustomToolbar,
-//   }}
-//   slotProps={ {toolbar: {searchInput, setSearchInput, setSearch}}}
-//   />
-
-
-
-// </Box>
-//     </Box>;
-// };
-
-// export default Transactions;
-
 import React, { useState } from "react";
 import { Box, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { useGetBalanceDataQuery, useGetBalanceOptionsQuery } from 'state/api';
+import { useGetBalanceDataQuery } from 'state/api';
 import Header from "components/Header";
 import DataGridCustomToolbar from "components/DataGridCustomToolbar";
 
@@ -133,12 +15,13 @@ const Transactions = () => {
   const [search, setSearch] = useState("");
 
   const [searchInput, setSearchInput] = useState("");
-  const { data, isLoading } = useGetBalanceDataQuery({
+  const { data, isLoading, error }  = useGetBalanceDataQuery({
     page,
     pageSize,
     sort: JSON.stringify(sort),
     search,
   });
+
 
   const columns = [
     {
@@ -197,8 +80,8 @@ const Transactions = () => {
       >
         <DataGrid
           loading={isLoading || !data}
-          getRowId={(row) => row.cik}  // Assuming CIK is unique
-          rows={data && data.transactions || []}  // Adjust if the data structure is different
+          rows={data || []}
+          getRowId={(row, index) => `${row.cik}-${index}-${row.fiscal_date_ending}`}
           columns={columns}
           rowCount={data && data.total || 0}
           rowsPerPageOptions={[20, 50, 100]}
@@ -209,7 +92,10 @@ const Transactions = () => {
           sortingMode="server"
           onPageChange={(newPage) => setPage(newPage)}
           onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+          onSortModelChange={(newSortModel) => {
+            console.log("New sort model:", newSortModel);
+            setSort(newSortModel);
+          }}
           slots={{
             toolbar: DataGridCustomToolbar,
           }}
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,47 +1,15 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-// export const api = createApi({
-//   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
-//   reducerPath: "adminApi",
-//   tagTypes: ["User", " Transactions"],
-//   endpoints: (build) => ({
-//     getUser: build.query({
-//       query: (id) => `general/user/${id}`,
-//       providesTags: ["User"],
-//     }),
-//     getTransactions: build.query({
-//       query: ({ page, pageSize, sort, search }) => ({
-//         url: "client/transactions",
-//         method: "GET",
-//         params: { page, pageSize, sort, search },
-//       }),
-//       providesTags: ["Transactions"],
-//     }),
-
-//   }),
-// });
-
-// export const {
-//   useGetUserQuery,
-//   useGetTransactionsQuery,
-// } = api;
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["BalanceOptions", "BalanceData"],  // Adjusted the tagTypes to match the data being fetched
+  tagTypes: [ "BalanceData"],  // Adjusted the tagTypes to match the data being fetched
   endpoints: (build) => ({
-    getBalanceOptions: build.query({  // Renamed to getBalanceOptions to match the endpoint
-      query: () => `fa-balance/options`,
-      providesTags: ["BalanceOptions"],
-    }),
     getBalanceData: build.query({  // Renamed to getBalanceData to match the endpoint
-      query: ({ period, ticker }) => ({
+      query: () => ({
         url: `fa-balance/`,
-        method: "GET",
-        params: { period, ticker },
+        method: "GET"
       }),
       providesTags: ["BalanceData"],
     }),
@@ -49,6 +17,5 @@ export const api = createApi({
 });
 
 export const {
-  useGetBalanceOptionsQuery,
   useGetBalanceDataQuery,
 } = api;
